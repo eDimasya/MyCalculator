a3a23f63052c36b73ac584cc27a1b43a1ae0be6f
Dima Egorov <edimasya@gmail.com>
2020-03-10 16:26:01 +0700

    same test

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.mycalculator
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun incNumberFromString(view: View){
+        var numberStr: String = InfixNotation_txtEdit.text.toString()
+        // var numb: Int = numberStr.toInt()
+        // var res: Int = numb + numb
+        Result_txt.text = numberStr.toString()
     }
 
 }
