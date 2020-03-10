44511e3658a99eceb5ded88bb56a9c8649dc8067
Dima Egorov <edimasya@gmail.com>
2020-03-10 15:50:58 +0700

    added textView and buttons for infix calc

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.mycalculator
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,26 +10,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun toastMe (view: View) {
-        val myToast = Toast.makeText(this, "Hello, Toast!", Toast.LENGTH_SHORT)
-
-        myToast.show()
-    }
-
-    fun countMe (view: View){
-        val countString = textView_Value.text.toString()
-
-        var count: Int = Integer.parseInt(countString)
-        count++
-        textView_Value.text = count.toString()
-    }
-
-    /*fun randomMe (view: View){
-        val randomIntent = Intent(this, secondActivity::class.java)
-        val countString = textView_Value.text.toString()
-        val count = Integer.parseInt(countString)
-        randomIntent.putExtra(secondActivity.TOTAL_COUNT, count)
-        startActivity(randomIntent)
-
-    }*/
 }
